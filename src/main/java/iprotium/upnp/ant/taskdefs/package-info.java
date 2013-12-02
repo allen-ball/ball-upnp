6631ed772bf606e3be37287584728fb4506a0401
Allen D. Ball <ball@hcf.dev>
2013-12-02 15:04:00 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.upnp/trunk@1261 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/upnp/ant/taskdefs/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * See {@link org.apache.tools.ant.taskdefs}.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ */
+@ManifestSection
+package iprotium.upnp.ant.taskdefs;
+
+import iprotium.annotation.ManifestSection;
