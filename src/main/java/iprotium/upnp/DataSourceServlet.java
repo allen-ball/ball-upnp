8601b984b674f218a1dc6fb1b848df3f1df5d904
Allen D. Ball <ball@hcf.dev>
2013-12-27 16:46:33 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.upnp/trunk@1309 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/upnp/DataSourceServlet.java
@@ -0,0 +1,70 @@
+/*
+ * $Id$
+ *
+ * Copyright 2013 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.upnp;
+
+import iprotium.io.IOUtil;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.activation.DataSource;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * {@link HttpServlet} implementation to serve a {@link DataSource}.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+public class DataSourceServlet extends HttpServlet {
+    private static final long serialVersionUID = 360854166101066804L;
+
+    private DataSource ds = null;
+
+    /**
+     * Sole constructor.
+     *
+     * @param   ds              The {@link DataSouce}.
+     */
+    public DataSourceServlet(DataSource ds) {
+        super();
+
+        setDataSource(ds);
+    }
+
+    public DataSource getDataSource() { return ds; }
+    public void setDataSource(DataSource ds) { this.ds = ds; }
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws IOException,
+                                                              ServletException {
+        DataSource ds = getDataSource();
+
+        if (ds != null) {
+            InputStream in = null;
+            ServletOutputStream out = null;
+
+            try {
+                if (ds.getContentType() != null) {
+                    response.setContentType(ds.getContentType());
+                }
+
+                in = ds.getInputStream();
+                out = response.getOutputStream();
+                IOUtil.copy(in, out);
+            } finally {
+                IOUtil.close(in);
+                IOUtil.close(out);
+            }
+        }
+    }
+
+    @Override
+    public String toString() { return getClass().getSimpleName(); }
+}
