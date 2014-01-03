8ffabbbf77fdaf5d523157bfa7118d537de24745
Allen D. Ball <ball@hcf.dev>
2014-01-03 17:40:43 +0000

    Added <tomcat/> build target and Ant Task.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.upnp/trunk@1364 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/upnp/ant/taskdefs/TomcatTask.java
@@ -0,0 +1,82 @@
+/*
+ * $Id$
+ *
+ * Copyright 2014 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.upnp.ant.taskdefs;
+
+import iprotium.annotation.AntTask;
+import iprotium.util.Factory;
+import iprotium.util.ant.taskdefs.AbstractClasspathTask;
+import java.io.File;
+import org.apache.catalina.Server;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.tools.ant.BuildException;
+
+/**
+ * {@link.uri http://ant.apache.org/ Ant} {@link org.apache.tools.ant.Task}
+ * to run a {@link Tomcat} instance.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+@AntTask("tomcat")
+public class TomcatTask extends AbstractClasspathTask {
+    private String type = Tomcat.class.getName();
+    private File basedir = null;
+    private Integer port = null;
+
+    /**
+     * Sole constructor.
+     */
+    public TomcatTask() { super(); }
+
+    protected String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    protected File getBasedir() { return basedir; }
+    public void setBasedir(File basedir) { this.basedir = basedir; }
+
+    protected Integer getPort() { return port; }
+    public void setPort(Integer port) { this.port = port; }
+
+    @Override
+    public void execute() throws BuildException {
+        if (getPort() == null) {
+            throw new BuildException("`port' attribute must be specified");
+        }
+
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+
+        try {
+            Thread.currentThread().setContextClassLoader(getClassLoader());
+
+            Class<? extends Tomcat> type =
+                Class.forName(getType()).asSubclass(Tomcat.class);
+            Tomcat tomcat = new Factory<Tomcat>(type).getInstance();
+
+            if (getBasedir() != null) {
+                tomcat.setBaseDir(getBasedir().getAbsolutePath());
+            }
+
+            tomcat.setPort(getPort());
+            tomcat.getServer().setParentClassLoader(getClassLoader());
+            tomcat.start();
+
+            if (tomcat.getServer().getPort() > 0) {
+                log(String.valueOf(tomcat.getServer().getAddress())
+                    + ":" + String.valueOf(tomcat.getServer().getPort())
+                    + " " + String.valueOf(tomcat.getServer().getShutdown()));
+            }
+
+            tomcat.getServer().await();
+        } catch (BuildException exception) {
+            throw exception;
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            throw new BuildException(throwable);
+        } finally {
+            Thread.currentThread().setContextClassLoader(loader);
+        }
+    }
+}
