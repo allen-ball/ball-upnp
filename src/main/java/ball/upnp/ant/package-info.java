bbd6f3d0f20a964575b1da4ef4e7d9f88497abde
Allen D. Ball <ball@hcf.dev>
2016-04-24 19:21:33 +0000

    Added antlib.xml descriptor to JAR.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.upnp/trunk@1901 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/upnp/ant/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * See {@link org.apache.tools.ant}.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ */
+@Manifest.Section
+@AntLib
+package ball.upnp.ant;
+
+import ball.annotation.Manifest;
+import ball.util.ant.taskdefs.AntLib;
