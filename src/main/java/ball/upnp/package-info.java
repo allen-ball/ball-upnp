862f4d570db56d1ed81fdcd6140fc4cbb9a5fe26
Allen D. Ball <ball@hcf.dev>
2020-02-05 04:23:21 +0000

    Generated license-maven-plugin:update-file-header headers.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.upnp/trunk@5285 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/upnp/package-info.java
+++ src/main/java/ball/upnp/package-info.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright 2016 - 2020 Allen D. Ball.  All rights reserved.
- */
 /**
  * Classes specific to
  * {@link.uri http://www.upnp.org/ Universal Plug and Play (UPnP)}
@@ -14,5 +9,25 @@
  */
 @Manifest.Section
 package ball.upnp;
-
+/*-
+ * ##########################################################################
+ * UPnP/SSDP Implementation Classes
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2013 - 2020 Allen D. Ball
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ##########################################################################
+ */
 import ball.annotation.Manifest;
