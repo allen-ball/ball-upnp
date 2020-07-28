d4b16d6f07be41ebe7a404deebadaecbf08c24df
Allen D. Ball <ball@hcf.dev>
2020-07-28 05:04:38 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball-upnp/trunk@6547 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/upnp/RootDevice.java
@@ -0,0 +1,37 @@
+package ball.upnp;
+/*-
+ * ##########################################################################
+ * UPnP/SSDP Implementation Classes
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2013 - 2020 Allen D. Ball
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ##########################################################################
+ */
+import java.net.URI;
+
+/**
+ * {@link.uri http://www.upnp.org/ UPnP} {@link RootDevice} interface.
+ *
+ * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
+ * @version $Revision$
+ */
+public interface RootDevice extends Device {
+
+    /**
+     * {@code upnp:rootdevice}
+     */
+    public static final URI NT = URI.create("upnp:rootdevice");
+}
