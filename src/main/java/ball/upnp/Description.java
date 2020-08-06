724623c5372c7d484a732293600676b9bad1896e
Allen D. Ball <ball@hcf.dev>
2020-08-06 15:31:28 +0000

    Removed getSpecVersion() method.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball-upnp/trunk@6588 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/upnp/Description.java
+++ src/main/java/ball/upnp/Description.java
@@ -20,16 +20,8 @@ package ball.upnp;
  * limitations under the License.
  * ##########################################################################
  */
-import ball.annotation.CompileTimeCheck;
 import ball.upnp.annotation.XmlNs;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.core.annotation.AnnotationUtils;
-
-import static java.util.Objects.requireNonNull;
-import static lombok.AccessLevel.PRIVATE;
 
 /**
  * {@link Description} support for {@link XmlNs} annotation.
@@ -38,15 +30,6 @@ import static lombok.AccessLevel.PRIVATE;
  * @version $Revision$
  */
 public interface Description {
-
-    /**
-     * {@link Pattern} to parse {@link #getXmlns()} to generate a
-     * {@link SpecVersion}.  Provides "{@code major}" and "{@code minor}"
-     * matching groups.
-     */
-    @CompileTimeCheck
-    public static final Pattern SPEC_VERSION_PATTERN =
-        Pattern.compile("(?i)^.+-(?<major>[0-9]+)-(?<minor>[0-9]+)$");
 
     /**
      * Method to get the {@link XmlNs}'s name.
@@ -58,34 +41,5 @@ public interface Description {
             AnnotationUtils.findAnnotation(getClass(), XmlNs.class);
 
         return (annotation != null) ? annotation.value() : null;
-    }
-
-    /**
-     * Method to generate {@link SpecVersion} from {@link XmlNs}.
-     *
-     * @return  The {@link SpecVersion}.
-     */
-    default SpecVersion getSpecVersion() {
-        SpecVersion version = null;
-        Matcher matcher = SPEC_VERSION_PATTERN.matcher(getXmlns());
-
-        if (matcher.matches()) {
-            version =
-                new SpecVersion(matcher.group("major"),
-                                matcher.group("minor"));
-        }
-
-        return version;
-    }
-
-    /**
-     * See {@link #getSpecVersion()}.
-     *
-     * {@bean.info}
-     */
-    @AllArgsConstructor(access = PRIVATE) @Data
-    public static class SpecVersion {
-        private String major = null;
-        private String minor = null;
     }
 }
